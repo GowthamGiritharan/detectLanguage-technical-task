88064b258e40c5465b0e7eb766b2e888422742cd
Gowtham Giritharan <gowtham.giritharan@starstable.com>
2023-11-27 22:59:01 +0100

    Update tests

--- cypress/e2e/detect_language.cy.js
+++ cypress/e2e/detect_language.cy.js
@@ -2,7 +2,17 @@
 
 describe("Detect language endpoint", () => {
   it("should throw error for an invalid api key", () => {
+    //check401SatusCodUnauthorized is a custom function written in support/commands.js
     cy.check401SatusCodUnauthorized("POST", Cypress.env("detect_url"));
+  });
+
+  it("should respond success for a valid input", () => {
+    cy.request({
+      method: "POST",
+      url: Cypress.env("detect_url"),
+      headers: { Authorization: "Bearer " + Cypress.env("api_key") },
+      body: { q: "tjena" },
+    }).its('status').should('eq', 200)
   });
 
   it("should detect single language", () => {
@@ -12,12 +22,20 @@ describe("Detect language endpoint", () => {
       headers: { Authorization: "Bearer " + Cypress.env("api_key") },
       body: { q: "tjena" },
     }).then((response) => {
-      expect(response.status).to.eq(200);
       const responseBody = response.body.data.detections[0];
       expect(responseBody.language).to.eq("sv");
       expect(responseBody.isReliable).to.eq(true);
       expect(responseBody.confidence).to.be.a("number");
     });
+  });
+
+  it("should return multiple results when keyphrase is ambiguous", () => {
+    cy.request({
+      method: "POST",
+      url: Cypress.env("detect_url"),
+      headers: { Authorization: "Bearer " + Cypress.env("api_key") },
+      body: { q: "papa" },
+    }).its('body.data.detections').should('have.length', 3)
   });
 
   it("should detect a batch", () => {
@@ -27,7 +45,6 @@ describe("Detect language endpoint", () => {
       headers: { Authorization: "Bearer " + Cypress.env("api_key") },
       body: { q: ["வணக்கம்", "greetings"] },
     }).then((response) => {
-      expect(response.status).to.eq(200);
       const responseBody = response.body.data.detections;
       expect(responseBody[0][0].language).to.eq("ta");
       expect(responseBody[1][0].language).to.eq("en");
@@ -41,7 +58,6 @@ describe("Detect language endpoint", () => {
         headers: { Authorization: "Bearer " + Cypress.env("api_key") },
         body: { q: "!§" },
       }).then((response) => {
-        expect(response.status).to.eq(200);
         const responseBody = response.body.data.detections;
         expect(responseBody).to.be.empty;
       });
--- cypress/e2e/user_status.cy.js
+++ cypress/e2e/user_status.cy.js
@@ -1,26 +1,33 @@
 /// <reference types="cypress" />
 
 describe("User status endpoint", () => {
-
-    it("should throw error for an invalid api key", () => {
-        cy.check401SatusCodUnauthorized("GET", Cypress.env("user_status_url"));
-      });
-    
-    it("should respond with user status", () => {
-      const today = new Date();
-      const todaysDate = today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
-  
-      cy.request({
-        method: "GET",
-        url: Cypress.env("user_status_url"),
-        headers: { Authorization: "Bearer " + Cypress.env("api_key") },
-        body: {},
-      }).then((response) => {
-        expect(response.status).to.eq(200);
-        console.log(response);
-        const responseBody = response.body;
-        expect(responseBody.date).to.eq(todaysDate);
-        expect(responseBody.status).to.eq("ACTIVE");
-      });
-    });    
+  it("should throw error for an invalid api key", () => {
+    cy.check401SatusCodUnauthorized("GET", Cypress.env("user_status_url"));
   });
+
+  it("should respond success for a valid input", () => {
+    cy.request({
+      method: "GET",
+      url: Cypress.env("user_status_url"),
+      headers: { Authorization: "Bearer " + Cypress.env("api_key") },
+      body: {},
+    })
+      .its("status").should("eq", 200);
+  });
+
+  it("should respond with user status", () => {
+    const today = new Date();
+    const todaysDate = today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+
+    cy.request({
+      method: "GET",
+      url: Cypress.env("user_status_url"),
+      headers: { Authorization: "Bearer " + Cypress.env("api_key") },
+      body: {},
+    }).then((response) => {
+      const responseBody = response.body;
+      expect(responseBody.date).to.eq(todaysDate);
+      expect(responseBody.status).to.eq("ACTIVE");
+    });
+  });
+});
